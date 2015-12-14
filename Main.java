d5324b5768e8b434e0a6509636224badcdf32814
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-12-13 21:11:46 -0500

    initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+//TODO: <name>
+//TODO: <date>
+//Practice 2015 - 05: //TODO
+
+public class  {
+  public static void main(String[] args) {
+
+    System.exit(0);
+  }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Practice 2015 - 05
+
+## Background
+
+## Description
+
+### Input
+
+### Output
+
+## Sample
+### Input
+```
+```
+
+### Output
+```
+```
